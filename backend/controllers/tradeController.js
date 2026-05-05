7b0145401c39f56e1e394f8ac1f8fa0bdb3746cd
RJ-1508 <rishabh31415@gmail.com>
2026-05-06 03:22:37 +0530

    Add sell endpoint with holdings update and deletion

--- backend/controllers/tradeController.js
+++ backend/controllers/tradeController.js
@@ -35,7 +35,7 @@ const buy = async (req, res) => {
                 throw new Error('Insufficient funds')
             }
 
-            const existingholding = await tx.holding.findUnique({
+            const existingHolding = await tx.holding.findUnique({
                 where: {
                     portfolioId_ticker: {
                         portfolioId: portfolio.id,
@@ -44,9 +44,9 @@ const buy = async (req, res) => {
                 }    
             })
             let newQty, newAvgPrice;
-            if (existingholding) {
-                const oldQty = Number(existingholding.quantity)
-                const oldAvg = Number(existingholding.avgPrice)
+            if (existingHolding) {
+                const oldQty = Number(existingHolding.quantity)
+                const oldAvg = Number(existingHolding.avgPrice)
 
                 newQty = oldQty + shares;
                 newAvgPrice = ((oldQty * oldAvg) + totalCost) / newQty;
@@ -114,4 +114,99 @@ const buy = async (req, res) => {
     }
 }
 
-module.exports = { buy }
+const sell = async (req, res) => {
+    try {
+        const { ticker, shares } = req.body;
+
+        if (!ticker || !shares || shares <= 0) {
+            return res.status(400).json({ error: 'Missing fields' });
+        };
+
+        const result = await prisma.$transaction(async (tx) => {
+            const portfolio = await tx.portfolio.findUnique({
+                where: { userId: req.userId },
+            });
+
+            if (!portfolio) {
+                throw new Error('Portfolio not found')
+            }
+
+            const existingHolding = await tx.holding.findUnique({
+                where: { portfolioId_ticker: { portfolioId: portfolio.id, ticker } }
+            })
+
+            if (!existingHolding) {
+                throw new Error('No holding')
+            }
+
+            if (existingHolding.quantity < shares ) {
+                throw new Error('Not enough stocks')
+            }
+
+            const response = await axios.get('https://finnhub.io/api/v1/quote', {
+                params: {
+                    symbol: ticker,
+                    token: process.env.FINNHUB_API_KEY
+                }
+            });
+
+            const qty = Number(existingHolding.quantity)
+            const avg = Number(existingHolding.avgPrice)
+            const sellPrice = response.data.c
+            const totalValue = sellPrice * shares
+            const newQty = qty - shares
+
+            if (newQty === 0) {
+                await tx.holding.delete({
+                    where: { portfolioId_ticker: { portfolioId: portfolio.id, ticker } },
+                })
+            } else {
+                await tx.holding.update({
+                    where: { portfolioId_ticker: { portfolioId: portfolio.id, ticker } },
+                    data: { quantity: newQty }
+                })
+            }
+            const balance = Number(portfolio.cashBalance)
+            const newBalance = balance + totalValue
+            await tx.portfolio.update({
+                where: {id: portfolio.id},
+                data: {
+                    cashBalance: newBalance
+                }
+            });
+
+            const transactionRecord = await tx.transaction.create({
+                data: {
+                    portfolioId: portfolio.id,
+                    ticker,
+                    type: 'SELL',
+                    quantity: shares,
+                    price: sellPrice,
+                    totalAmount: totalValue
+                }
+            })
+            return transactionRecord
+        })
+
+        return res.status(200).json({
+            message: 'Sale successful',
+            transaction: result
+        })
+    } catch (error) {
+        if (error.message === 'Portfolio not found') {
+            return res.status(404).json({ error: 'Portfolio not found' });
+        }
+
+        if (error.message === 'No holding') {
+            return res.status(404).json({ error: 'No such holding exists'})
+        }
+
+        if (error.message === 'Not enough stocks') {
+            return res.status(400).json({ error : 'Not enough shares to sell'})
+        }
+
+        console.error(error)
+        return res.status(500).json({ error : 'Something went wrong'})
+    }
+}
+module.exports = { buy, sell }
--- backend/routes/trade.js
+++ backend/routes/trade.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = new express.Router()
-const { buy } = require('../controllers/tradeController')
+const { buy, sell } = require('../controllers/tradeController')
 const authenticate = require('../middleware/auth')
 
 router.post('/buy', authenticate, buy)
+router.post('/sell', authenticate, sell)
 module.exports = router
